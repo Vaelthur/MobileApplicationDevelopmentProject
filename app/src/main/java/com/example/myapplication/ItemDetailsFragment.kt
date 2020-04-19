272fbb1dfebd6bcc6fbe789fd1e45a484d786283
SilvioMarcato <s258037@studenti.polito.it>
2020-04-19 14:11:25 +0200

    Step 3 done

---
+++ app/src/main/java/com/example/myapplication/ItemDetailsFragment.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+
+class ItemDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ItemDetailsFragment()
+    }
+
+    private lateinit var viewModel: ItemDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.item_details_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ItemDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.item_details_menu, menu)
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ItemDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+import androidx.lifecycle.ViewModel
+
+class ItemDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
