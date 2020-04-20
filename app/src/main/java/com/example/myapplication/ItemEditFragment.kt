cafe5d389e2eb53308c86c2659b9c96eb5dfe28c
Giorgio Maritano <giorgio@maritano.it>
2020-04-20 16:21:47 +0200

    edit item fragment - doesnt crash anymore

--- app/src/main/java/com/example/myapplication/ItemEditFragment.kt
+++ app/src/main/java/com/example/myapplication/ItemEditFragment.kt
@@ -2,9 +2,10 @@ package com.example.myapplication
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.text.InputType
 import android.view.*
-import androidx.fragment.app.Fragment
 import android.widget.EditText
+import androidx.fragment.app.Fragment
 import java.util.*
 
 class ItemEditFragment : Fragment() {
@@ -19,16 +20,17 @@ class ItemEditFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val eText = R.id.item_expire_date_value as EditText
+        val view = inflater.inflate(R.layout.fragment_item_edit, container, false)
+        val eText = view.findViewById<EditText>(R.id.item_expire_date_value)
         eText.setOnClickListener {
             val cal = Calendar.getInstance()
             val y = cal.get(Calendar.YEAR)
             val m = cal.get(Calendar.MONTH)
             val d = cal.get(Calendar.DAY_OF_MONTH)
-            // show just datepicker (i hope)
+            // datepicker doesnt show up
             context?.let { it1 -> DatePickerDialog(it1, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                //TODO: save inserted parameters and show into textedit
+                val t = "$dayOfMonth/$month/$year"
+                eText.setText(t)
             }, y, m , d ) }?.show()
         }
         return inflater.inflate(R.layout.fragment_item_edit, container, false)
