3632242443809eff3116ef8e4b4d0898752c0329
dev-albo <s256659@studenti.polito.it>
2020-04-22 15:04:14 +0200

    permissions asked - to do all camera and gallery - currently not working

--- app/src/main/java/com/example/myapplication/profile/EditProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/profile/EditProfileFragment.kt
@@ -1,15 +1,26 @@
 package com.example.myapplication.profile
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.*
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders.of
@@ -20,9 +31,20 @@ import com.example.myapplication.Helpers
 import com.example.myapplication.R
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_edit_profile.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class EditProfileFragment : Fragment() {
+
+    //requests and permissions codes
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_IMAGE_GALLERY = 2
+    private val PERMISSION_CODE_CAMERA = 1000
+    private val PERMISSION_CODE_GALLERY = 1001
 
     private lateinit var showProfileViewModel: ShowProfileViewModel
 
@@ -99,6 +121,24 @@ class EditProfileFragment : Fragment() {
         }
     }
 
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+
+            R.id.takePicture -> {
+                takePictureHandler()
+                true
+            }
+
+            R.id.selectFromGallery -> {
+                selectFromGalleryHandler()
+                true
+            }
+            // Default case
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
     private fun saveProfile() {
 
         hideSoftKeyboard(this.activity)
@@ -126,12 +166,6 @@ class EditProfileFragment : Fragment() {
 
         showProfileViewModel.setAccountInfo(accountInfo)
         //Return to ShowProfileActivity
-        /*val startProfileEditIntent = Intent()
-        val accountBundle = Bundle()
-        accountBundle.putSerializable("accountInfo", accountInfo)
-        startProfileEditIntent.putExtras(accountBundle)
-        setResult(Activity.RESULT_OK ,startProfileEditIntent)*/
-        //(this.activity as AppCompatActivity).onBackPressed()
         this.activity?.findNavController(R.id.nav_host_fragment)?.popBackStack()
     }
 
@@ -146,6 +180,159 @@ class EditProfileFragment : Fragment() {
                     0
                 )
             }
+        }
+    }
+
+    /// region Camera functions
+
+    private fun takePictureHandler(){
+
+        val permissions: Array<String> = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        if (checkCamera()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                openCamera(permissions)
+            } else {
+                //permission already granted or version in below marshmallow
+                takePicture()
+            }
+        } else {
+            Toast.makeText(context, "Camera not found", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkCamera() : Boolean{
+        return context?.packageManager?.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)!!
+    }
+
+    private fun openCamera(permissions: Array<String>? = null) {
+
+        permissions?.let{
+            // Take picture is already called in function onRequestPermissionsResult(), so we return
+            checkAndAskPermissions(permissions, this.PERMISSION_CODE_CAMERA)
+            return
+        }
+
+        takePicture()
+    }
+
+    private fun takePicture(){
+
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val photoFile : File = createImageFile()
+        val photoUri = FileProvider.getUriForFile(requireContext(), "MAD.shout.lab2.provider", photoFile)
+
+        // Add photo_uri string to shared preferences
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putString("profile_picture_editing", photoUri.toString())
+            commit()
+        }
+
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    /// endregion
+
+    /// region Gallery functions
+
+    private fun selectFromGalleryHandler(){
+
+        val permissions: Array<String> = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            openGallery(permissions)
+        }
+        else {
+            //permission already granted or version in below marshmallow
+            takeFromGallery()
+        }
+    }
+
+    private fun openGallery(permissions: Array<String>? = null) {
+
+        permissions?.let{
+            // Take picture is already called in function onRequestPermissionsResult(), so we return
+            checkAndAskPermissions(permissions, this.PERMISSION_CODE_GALLERY)
+            return
+        }
+
+        takeFromGallery()
+    }
+
+    private fun takeFromGallery() {
+
+        val galleryIntent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        galleryIntent.type = "image/*"
+        val mimeTypes = arrayOf("image/jpeg", "image/png")
+        galleryIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        startActivityForResult(galleryIntent,  REQUEST_IMAGE_GALLERY)
+    }
+
+    /// endregion
+
+    /// region Helpers
+
+    private fun createImageFile(): File {
+
+        lateinit var file : File
+
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        try {
+            val storageDir: File? = activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            file = File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir)
+        }
+        catch (e: IOException){
+            e.printStackTrace()
+            Toast.makeText(activity, "Could not set profile picture", Toast.LENGTH_SHORT).show()
+        }
+
+        return file
+    }
+
+    @SuppressLint("NewApi")
+    private fun checkAndAskPermissions(permissions: Array<String>, permissionCode : Int){
+
+        val permissionsToRequest = ArrayList<String>()
+        val createJavaArray = {index: Int -> permissionsToRequest[index] }
+
+        for( permission in permissions){
+            if(ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_DENIED){
+                permissionsToRequest.add(permission)
+            }
+        }
+
+        if(permissionsToRequest.isNotEmpty()) {
+            this.requestPermissions(Array<String>(permissionsToRequest.size, createJavaArray),
+                permissionCode)
+        }
+        else {
+            when(permissionCode) {
+                PERMISSION_CODE_CAMERA -> takePicture()
+                PERMISSION_CODE_GALLERY -> takeFromGallery()
+            }
+        }
+    }
+
+    /// endregion
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        //called when user presses ALLOW or DENY from Permission Request Popup
+
+        for (result in grantResults){
+            if(result != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this.context, "Permission denied", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+
+        when(requestCode){
+            PERMISSION_CODE_CAMERA -> takePicture()
+            PERMISSION_CODE_GALLERY -> takeFromGallery()
         }
     }
 
