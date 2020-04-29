9b2e49dd3937c45c2cb42b4e25c7557a0fa7d8d1
Giorgio Maritano <giorgio@maritano.it>
2020-04-29 14:15:43 +0200

    fixed rotation drawer issue TODO: hamburger

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.View
 import android.widget.LinearLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
@@ -27,14 +30,14 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         // value took from values/attributes, different foreach layout, so it picks the correct one
-        val isTablet: Boolean = resources.getBoolean(R.bool.isTablet)
+        val isTabletLandscape: Boolean = resources.getBoolean(R.bool.isTablet)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 
         // check the device type and shows drawer accordingly
-        if (isTablet) {
-            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN)
+        if (isTabletLandscape) {
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
             drawerLayout.setScrimColor(0x00000000) // set the rest of the screen without shadow as if drawer and the rest were on the same level
-        } else drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        }
 
 
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -71,6 +74,4 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-
 }
