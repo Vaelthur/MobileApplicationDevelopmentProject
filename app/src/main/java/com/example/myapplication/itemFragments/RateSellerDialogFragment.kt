1b03d6b7f5a554edf74bb4bb3a087fd67d807e94
dev-albo <s256659@studenti.polito.it>
2020-05-26 12:21:05 +0200

    User ratings

---
+++ app/src/main/java/com/example/myapplication/itemFragments/RateSellerDialogFragment.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication.itemFragments
+
+import androidx.fragment.app.DialogFragment
+import android.app.Dialog
+import android.app.SearchManager
+import android.media.Rating
+import android.os.Bundle
+import android.widget.RatingBar
+import androidx.appcompat.app.AlertDialog
+import com.example.myapplication.R
+import com.example.myapplication.data.ItemCategories
+import com.example.myapplication.itemLists.FilterItemFragment
+
+class RateSellerDialogFragment(itemDetailsFragment: ItemDetailsFragment, val ownerId : String) : DialogFragment() {
+
+    private val title : String  = "Please rate the seller"
+    private var listener: RateSellerListener = itemDetailsFragment
+
+    interface RateSellerListener {
+        fun onDialogPositiveClick(dialog: DialogFragment, rating: Float?, ownerId: String)
+        fun onDialogNegativeClick(dialog: DialogFragment)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(title)
+            val ratingView = layoutInflater.inflate(R.layout.rating_view, null)
+            builder.setView(ratingView)
+            builder.setPositiveButton("OK")
+                { _, _ ->
+                    val ratingBar = ratingView.findViewById<RatingBar>(R.id.ratingBar)
+                    listener.onDialogPositiveClick(this, ratingBar.rating, ownerId)
+                }
+                .setNegativeButton("Not now")
+                { _, _ ->
+                    listener.onDialogNegativeClick(this)
+                }
+                .setOnCancelListener {
+                    listener.onDialogNegativeClick(this)
+                }
+
+            builder.setCancelable(true)
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
