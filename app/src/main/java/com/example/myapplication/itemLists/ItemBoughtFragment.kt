07cf47ff8e1ad3984420675f84d2943117fd7380
SilvioMarcato <s258037@studenti.polito.it>
2020-05-27 14:42:12 +0200

    fixed empty msg on bought fragment

--- app/src/main/java/com/example/myapplication/itemLists/ItemBoughtFragment.kt
+++ app/src/main/java/com/example/myapplication/itemLists/ItemBoughtFragment.kt
@@ -10,38 +10,52 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.data.FireItem
 import com.example.myapplication.itemFragments.ItemDetailsViewModel
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.item_bought_list.*
 
 class ItemBoughtFragment : Fragment() {
 
     private lateinit var itemDetailsViewModel: ItemDetailsViewModel
 
-    @Suppress("DEPRECATION")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        return inflater.inflate(R.layout.item_bought_list, container, false)
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         itemDetailsViewModel =
             ViewModelProviders.of(requireActivity()).get(ItemDetailsViewModel::class.java)
 
         itemDetailsViewModel.tempItemInfo.value = null
 
-
-        val root = inflater.inflate(R.layout.item_bought_list, container, false)
-
         itemDetailsViewModel.boughtItems(FirebaseAuth.getInstance().currentUser!!.uid)
 
         itemDetailsViewModel.boughtLiveData.observe(requireActivity(), Observer {
-            val recyclerView: RecyclerView? = root.findViewById(R.id.recyclerBoughtItemList)
+            val recyclerView: RecyclerView? = view.findViewById(R.id.recyclerBoughtItemList)
             recyclerView?.layoutManager = LinearLayoutManager(context)
 
             recyclerView?.adapter =
                 ItemInfoAdapter(it)
-        })
 
-        return root
+            if(view.isShown)
+                checkEmptyList(it as MutableList<FireItem>)
+        })
+    }
+
+    private fun checkEmptyList(itemList: MutableList<FireItem>) {
+        if(itemList.isEmpty()) {
+            empty_bought_list_msg.visibility = View.VISIBLE
+        } else {
+            empty_bought_list_msg.visibility = View.INVISIBLE
+        }
     }
 }
